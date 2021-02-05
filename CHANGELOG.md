9083fde7fea1f8926b5b5504190f53b48e4eed5b
Liam So <Liam.So@dal.ca>
2021-02-04 20:29:55 -0400

    Added Donate page

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+## 1.2
+
+* Added Donate Page (add to cart has no functionality yet)
+
+
 ## 1.1
 
 * Includes sidebar navigation, home wallpaper, and herosection.
--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Sidebar from './components/Sidebar/Sidebar'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './components/Pages/HomePage/Home'
+import Donate from './components/Pages/Donate/Donate'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Sidebar />
       <Switch>
           <Route path='/' exact component={Home} />
+          <Route path='/donate' exact component={Donate} />
       </Switch>
     </Router>
   );
