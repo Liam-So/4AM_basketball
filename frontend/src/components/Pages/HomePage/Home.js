42f477a36499ed99da2f13351b2181b3e2ea31e6
Liam So <Liam.So@dal.ca>
2021-02-28 22:53:31 -0400

    Updated home page

--- frontend/src/components/Pages/HomePage/Home.js
+++ frontend/src/components/Pages/HomePage/Home.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import {homeObjOne, homeObjTwo} from './Data'
 import HeroSection from '../../HeroSection/HeroSection'
+import { Button } from '@material-ui/core'
+import {Link} from 'react-router-dom'
 
 function Home() {
     return (
         <section>
-            <div className='home'></div>
+            <div className='home'>
+                4AM Basketball Camp
+                <Button variant="contained" style={{backgroundColor: '#fe4e50'}}>
+                    <Link to="/register" style={{color: "#fff", textDecoration: 'none'}}>
+                        REGISTER NOW
+                    </Link>
+                </Button>
+            </div>
             <HeroSection {...homeObjOne} />
             <HeroSection {...homeObjTwo} />
         </section>
