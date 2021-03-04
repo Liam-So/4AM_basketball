a229aa092a859b8fce1957cf8a4a4a2f9a494330
Ryan Hong <65246381+ryan-hong@users.noreply.github.com>
2021-03-03 20:40:09 -0400

    changed pre-existing sidebar to new topbar

--- frontend/src/components/Pages/Cart/Cart.js
+++ frontend/src/components/Pages/Cart/Cart.js
@@ -1,58 +1,76 @@
-import React from 'react'
-import { Container, Typography, Button, Grid } from '@material-ui/core'
-import useStyles from './styles'
-import CartItem from './CartItem/CartItem'
-import { useStateValue } from '../../StateProvider';
-import { getBasketTotal } from '../../reducer';
-import Sidebar from '../../Sidebar/Sidebar';
+import React from "react";
+import { Container, Typography, Button, Grid } from "@material-ui/core";
+import useStyles from "./styles";
+import CartItem from "./CartItem/CartItem";
+import { useStateValue } from "../../StateProvider";
+import { getBasketTotal } from "../../reducer";
+import Topbar from "../../Topbar/Topbar";
 
 function Cart() {
+  const classes = useStyles();
 
-    const classes = useStyles() ; 
+  const [{ basket }] = useStateValue();
 
-    const [{ basket }] = useStateValue() ; 
+  console.log(basket);
 
-    console.log(basket)
-
-    const EmptyCart = () => {
-        return <Typography variant="subtitle1" style={{height: '70vh', fontFamily: 'Lato'}}>You have no items in your shopping cart, start adding some!</Typography>
-    };
-
-
-    const FilledCart = () => (
-        <>            
-            <Grid container spacing={3}>
-                {Object.values(basket).map((item) => (
-                    <Grid item xs={12} sm={4} key={item.id}>
-                        <CartItem item={item} />
-                    </Grid>
-                ))}
-            </Grid>
-            <div className={classes.cardDetails}>
-                <Typography variant="h4"style={{fontFamily: 'Lato'}} >Subtotal: ${getBasketTotal(Object.values(basket))}</Typography>
-                <div style={{paddingBottom: '20px'}}>
-                    <Button className={classes.checkout} style={{fontFamily: 'Lato', backgroundColor: '#dc3545'}} size="large" type="button" variant ="contained" color="primary">
-                        Checkout
-                    </Button>
-                </div>
-            </div>
-
-        </>
-    )
-
-    if (Object.values(basket).length === undefined) return 'Loading...';
-
-
+  const EmptyCart = () => {
     return (
-        <>
-            <Sidebar />
-            <Container>
-                <Typography className={classes.title} variant="h3" gutterBottom style={{fontFamily: 'Lato'}}>Your Shopping Cart</Typography>
-                { Object.values(basket).length === 0  ? <EmptyCart /> : <FilledCart />}
-            </Container>
-        </>
-        
-    )
+      <Typography
+        variant="subtitle1"
+        style={{ height: "70vh", fontFamily: "Lato" }}
+      >
+        You have no items in your shopping cart, start adding some!
+      </Typography>
+    );
+  };
+
+  const FilledCart = () => (
+    <>
+      <Grid container spacing={3}>
+        {Object.values(basket).map((item) => (
+          <Grid item xs={12} sm={4} key={item.id}>
+            <CartItem item={item} />
+          </Grid>
+        ))}
+      </Grid>
+      <div className={classes.cardDetails}>
+        <Typography variant="h4" style={{ fontFamily: "Lato" }}>
+          Subtotal: ${getBasketTotal(Object.values(basket))}
+        </Typography>
+        <div style={{ paddingBottom: "20px" }}>
+          <Button
+            className={classes.checkout}
+            style={{ fontFamily: "Lato", backgroundColor: "#dc3545" }}
+            size="large"
+            type="button"
+            variant="contained"
+            color="primary"
+          >
+            Checkout
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+
+  if (Object.values(basket).length === undefined) return "Loading...";
+
+  return (
+    <>
+      <Topbar />
+      <Container>
+        <Typography
+          className={classes.title}
+          variant="h3"
+          gutterBottom
+          style={{ fontFamily: "Lato" }}
+        >
+          Your Shopping Cart
+        </Typography>
+        {Object.values(basket).length === 0 ? <EmptyCart /> : <FilledCart />}
+      </Container>
+    </>
+  );
 }
 
-export default Cart
+export default Cart;
--- frontend/src/components/Pages/Donate/Donate.js
+++ frontend/src/components/Pages/Donate/Donate.js
@@ -1,48 +1,43 @@
-import React, { useState, useEffect } from 'react'
-import { Grid } from '@material-ui/core'
-import DonateProduct from './DonateProduct/DonateProduct'
-import useStyles from './styles'
-import HeroSection from '../../HeroSection/HeroSection'
-import { homeObjOne } from './Data'
-import axios from '../../../axios'
-import Sidebar from '../../Sidebar/Sidebar'
+import React, { useState, useEffect } from "react";
+import { Grid } from "@material-ui/core";
+import DonateProduct from "./DonateProduct/DonateProduct";
+import useStyles from "./styles";
+import HeroSection from "../../HeroSection/HeroSection";
+import { homeObjOne } from "./Data";
+import axios from "../../../axios";
+import Topbar from "../../Topbar/Topbar";
 
 function Donate() {
+  const [products, setProducts] = useState([]);
 
-    const [products, setProducts] = useState([])
+  useEffect(() => {
+    async function fetchData() {
+      const req = await axios.get("/products");
 
-    useEffect(() => {
-        async function fetchData() {
-            const req = await axios.get('/products');
+      setProducts(req.data);
+    }
 
-            setProducts(req.data)
-        }
+    fetchData();
+  }, []);
 
-        fetchData() ;
-    }, [])
+  const classes = useStyles();
 
-
-    const classes = useStyles();
-
-
-    return (
-        <div>
-            <Sidebar />
-            <HeroSection {...homeObjOne} />
-            <main className={classes.content}>
-                <div className={classes.toolbar} />
-                <Grid container justify="center" spacing={4}>
-                    {products.map((product) => (
-                        <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                            <DonateProduct product={product}/>
-                        </Grid>
-                    ))}
-                </Grid>
-            </main>
-        </div>
-
-        
-    )
+  return (
+    <div>
+      <Topbar />
+      <HeroSection {...homeObjOne} />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Grid container justify="center" spacing={4}>
+          {products.map((product) => (
+            <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+              <DonateProduct product={product} />
+            </Grid>
+          ))}
+        </Grid>
+      </main>
+    </div>
+  );
 }
 
-export default Donate
+export default Donate;
