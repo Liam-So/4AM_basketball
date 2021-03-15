0e2177cebfa4789592c382fc870fe5a08ee00e83
Ryan Hong <65246381+ryan-hong@users.noreply.github.com>
2021-03-15 20:17:28 -0300

    added title and info section, changed form

---
+++ frontend/src/components/Pages/Scholarship/Heading.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+function Heading() {
+  return (
+    <div className="text-center sm: mx-10 md:mx-20">
+      <h1 className="mt-10 text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl">
+        Andrew Milner Memorial Scholarship
+      </h1>
+      <p className="mt-10 mb-10 text-base text-gray-700 sm:text-lg sm:max-w-7xl sm:mx-auto md:mt-20 md:text-xl md:mb-20">
+        The goal of the camp is to honour Andrew's legacy by paying his impact
+        forward to the next generation of basketball players. All of the
+        proceeds from the camp go towards an annual scholarhsip in Andrew's
+        name. <br />
+        <br />
+        <strong>
+          The 1st Annual 4AM camp was held in August 2019 and raised $4000 for
+          the very first Annual Andrew Milner Memorial Scholarship.
+        </strong>
+        <br />
+        <br />
+        The Milner/MacIntosh family has decided they would like to award this
+        scholarhsip to a BNS athlete. This scholarship is intended to help a BNS
+        athlete to alleviate the costs associated with playing for BNS. The
+        sholarship will be awarded to a male BNS athlete on either the U15 or
+        U17 team. To be a scholarship prospect, please fill out the form below.{" "}
+        <strong>
+          The recipient will be chosen based on the potential impact of the
+          scholarship.
+        </strong>
+      </p>
+    </div>
+  );
+}
+
+export default Heading;
--- frontend/src/components/Pages/Scholarship/Scholarship.js
+++ frontend/src/components/Pages/Scholarship/Scholarship.js
@@ -1,14 +1,16 @@
+import React from "react";
 import Topbar from "../../Topbar/Topbar";
+import Heading from "./Heading";
 import "./Scholarship.css";
 
-const Scholarship = () => {
+function Scholarship() {
   return (
     <div>
       <div className="header">
         <Topbar transparent={true} />
       </div>
-
-      <section className="bg-gray-100 max-w-7x1 mx-auto py-6 sm:px-6 lg:px-8">
+      <Heading />
+      <section className="bg-gray-100 max-w-7x1 mx-auto py-1 sm:py-5 lg:py-6 sm:px-6 lg:px-8">
         <div className="mt-10 sm:mt-0">
           <div className="md:grid md:grid-cols-3 md:gap-6">
             <div className="md:col-span-1">
@@ -28,7 +30,7 @@ const Scholarship = () => {
                     <div className="grid grid-cols-6 gap-6">
                       <div className="col-span-6 sm:col-span-3">
                         <label
-                          for="first_name"
+                          htmlFor="first_name"
                           className="block text-sm font-medium text-gray-700"
                         >
                           First name
@@ -43,7 +45,7 @@ const Scholarship = () => {
 
                       <div className="col-span-6 sm:col-span-3">
                         <label
-                          for="last_name"
+                          htmlFor="last_name"
                           className="block text-sm font-medium text-gray-700"
                         >
                           Last name
@@ -58,7 +60,7 @@ const Scholarship = () => {
 
                       <div className="col-span-6 sm:col-span-3">
                         <label
-                          for="email_address"
+                          htmlFor="email_address"
                           className="block text-sm font-medium text-gray-700"
                         >
                           Email address
@@ -73,7 +75,7 @@ const Scholarship = () => {
 
                       <div className="col-span-6 sm:col-span-3">
                         <label
-                          for="phone_number"
+                          htmlFor="phone_number"
                           className="block text-sm font-medium text-gray-700"
                         >
                           Phone Number
@@ -86,41 +88,9 @@ const Scholarship = () => {
                         />
                       </div>
 
-                      <div className="col-span-6 sm:col-span-3">
-                        <label
-                          for="country"
-                          className="block text-sm font-medium text-gray-700"
-                        >
-                          Country / Region
-                        </label>
-                        <select
-                          id="country"
-                          name="country"
-                          className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                        >
-                          <option>United States</option>
-                          <option>Canada</option>
-                        </select>
-                      </div>
-
-                      <div className="col-span-6">
-                        <label
-                          for="street_address"
-                          className="block text-sm font-medium text-gray-700"
-                        >
-                          Street address
-                        </label>
-                        <input
-                          type="text"
-                          name="street_address"
-                          id="street_address"
-                          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                        />
-                      </div>
-
                       <div className="col-span-6 sm:col-span-6 lg:col-span-2">
                         <label
-                          for="city"
+                          htmlFor="city"
                           className="block text-sm font-medium text-gray-700"
                         >
                           City
@@ -133,75 +103,69 @@ const Scholarship = () => {
                         />
                       </div>
 
-                      <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                      <div className="col-span-6 sm:col-span-6 lg:col-span-2">
                         <label
-                          for="state"
+                          htmlFor="school"
                           className="block text-sm font-medium text-gray-700"
                         >
-                          State / Province
+                          School
                         </label>
                         <input
                           type="text"
-                          name="state"
-                          id="state"
+                          name="school"
+                          id="school"
                           className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                         />
                       </div>
 
-                      <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                      <div className="col-span-6 sm:col-span-6 lg:col-span-2">
                         <label
-                          for="postal_code"
+                          htmlFor="team"
                           className="block text-sm font-medium text-gray-700"
                         >
-                          ZIP / Postal
+                          BNS Team
                         </label>
-                        <input
-                          type="text"
-                          name="postal_code"
-                          id="postal_code"
-                          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                        />
+                        <select
+                          id="team"
+                          name="team"
+                          className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                        >
+                          <option>U15</option>
+                          <option>U17</option>
+                        </select>
                       </div>
 
                       <div className="col-span-6">
-                        <label className="block text-sm font-medium text-gray-700">
-                          Upload File
+                        <label
+                          htmlFor="application"
+                          className="block text-sm font-medium text-gray-700"
+                        >
+                          Application Question
                         </label>
-                        <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
-                          <div className="space-y-1 text-center">
-                            <svg
-                              className="mx-auto h-12 w-12 text-gray-400"
-                              stroke="currentColor"
-                              fill="none"
-                              viewBox="0 0 48 48"
-                              aria-hidden="true"
-                            >
-                              <path
-                                d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                                stroke-width="2"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                              />
-                            </svg>
-                            <div className="flex text-sm text-gray-600">
-                              <label
-                                for="file-upload"
-                                className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
-                              >
-                                <span>Upload a file</span>
-                                <input
-                                  id="file-upload"
-                                  name="file-upload"
-                                  type="file"
-                                  className="sr-only"
-                                />
-                              </label>
-                              <p className="pl-1">or drag and drop</p>
-                            </div>
-                            <p className="text-xs text-gray-500">
-                              PDF, DOC up to 10MB
-                            </p>
-                          </div>
+                        <p className="mt-2 text-sm text-gray-500">
+                          Please share how this scholarship will impact you -
+                          you may speak to how it will impact you during the
+                          season with BNS this summer, how it will impact your
+                          future basketball journey, and/or how it will impact
+                          you as a person off the basketball court. <br />
+                          <br />
+                          We'd love to hear about your aspirations both on and
+                          off the court. Andrew embodied strong qualities of
+                          work ethic, leadership, humbleness, and gratitude,
+                          allowing him to be a champion on and off the court. We
+                          encourage you to highlight qualities like these in
+                          yourself. In the words of Andrew, "Never forget what
+                          made you who you are." (Max. 500 words).
+                        </p>
+                        <div className="mt-5">
+                          <textarea
+                            id="application"
+                            name="application"
+                            rows="10"
+                            className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md"
+                            placeholder="Your response here..."
+                            maxLength="3100"
+                          ></textarea>
                         </div>
                       </div>
                     </div>
@@ -223,6 +187,6 @@ const Scholarship = () => {
       </section>
     </div>
   );
-};
+}
 
 export default Scholarship;
