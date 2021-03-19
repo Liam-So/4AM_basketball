1e829ecbfabbbe58cd28228f897005cc84d73b0d
Ryan Hong <65246381+ryan-hong@users.noreply.github.com>
2021-03-19 16:25:50 -0300

    fixed hero section centering issue

--- frontend/src/components/HeroSection/HeroSection.js
+++ frontend/src/components/HeroSection/HeroSection.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import './HeroSection.css';
+import React from "react";
+import "./HeroSection.css";
 
 function HeroSection({
   lightBg,
@@ -10,41 +10,41 @@ function HeroSection({
   description,
   img,
   alt,
-  imgStart
+  imgStart,
 }) {
   return (
     <>
       <div
-        className={lightBg ? 'home__hero-section' : 'home__hero-section darkBg'}
+        className={lightBg ? "home__hero-section" : "home__hero-section darkBg"}
       >
-        <div className='container' >
+        <div className="flex items-center justify-center">
           <div
-            className='row home__hero-row'
+            className="row home__hero-row"
             style={{
-              display: 'flex',
-              flexDirection: imgStart === 'start' ? 'row-reverse' : 'row'
+              display: "flex",
+              flexDirection: imgStart === "start" ? "row-reverse" : "row",
             }}
           >
-            <div className='col'>
-              <div className='home__hero-text-wrapper'>
-                <div className='top-line'>{topLine}</div>
-                <h1 className={lightText ? 'heading' : 'heading dark'}>
+            <div className="col">
+              <div className="home__hero-text-wrapper">
+                <div className="top-line">{topLine}</div>
+                <h1 className={lightText ? "heading" : "heading dark"}>
                   {headline}
                 </h1>
                 <p
                   className={
                     lightTextDesc
-                      ? 'home__hero-subtitle'
-                      : 'home__hero-subtitle dark'
+                      ? "home__hero-subtitle"
+                      : "home__hero-subtitle dark"
                   }
                 >
                   {description}
                 </p>
               </div>
             </div>
-            <div className='col'>
-              <div className='home__hero-img-wrapper'>
-                <img src={img} alt={alt} className='home__hero-img' />
+            <div className="col">
+              <div className="home__hero-img-wrapper">
+                <img src={img} alt={alt} className="home__hero-img" />
               </div>
             </div>
           </div>
