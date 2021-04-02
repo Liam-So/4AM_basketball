6c8bfa5ff0b7ee79a72959e259b9064028c8eb7e
Ryan Hong <65246381+ryan-hong@users.noreply.github.com>
2021-04-02 17:01:00 -0300

    added new nodemailer tokens, and changed scholarship page

--- backend/config.js
+++ backend/config.js
@@ -4,9 +4,9 @@ const config = {
     "219916068332-l4r418pl5pm48i923fjsn3ehfg23v834.apps.googleusercontent.com",
   client_secret: "cmiUQMGf8Lvspuhw7_m6tTPT",
   refresh_token:
-    "1//04Ci5UTZ4YwdqCgYIARAAGAQSNwF-L9IrNYlr0Cvkrro8IuDIFNHFX6Nch-dE2Azy3yifXzHTHZt2zZ1RwM4-XRFyFlk0TFFhZdM",
+    "1//04ZwN3wpEFWaPCgYIARAAGAQSNwF-L9IrnMz4umKPKdqeChmq-PCr9rRm0RkgKbAIzxTDcpMO9njIcVbJb6zkZIharEJMYfOCyK0",
   access_token:
-    "ya29.a0AfH6SMB5nIsR_RM3hQgeBtcWyBQWPUH-VF_MypqHs9BQUkBLGBeof1aQn2OdKEtoRGc0UgpvyYOK-rB2JGdZzgMkFDvnA4WyLiP1Xvt58ELvMmk7Z9WswxDwGmjonmb8zhY2_PMnaxnSFzPaPglBSa05bWWg",
+    "ya29.a0AfH6SMCgCc9fb4koB1OFRY3L8vPgyVgSb1L8BjGbyj5xbTqBhxfgCF8wUgHBLwIwFUezh6lD2txXu2iY-tZYUk1hrd16xMEWQOB1oNjbt08Bc7uoTcnH-uUCZs3gUeVJx7jOuiXJL7rKCeiwQlMGVK3xlHQz",
 };
 
 export default config;
--- frontend/src/components/Pages/Scholarship/Scholarship.js
+++ frontend/src/components/Pages/Scholarship/Scholarship.js
@@ -53,184 +53,172 @@ function Scholarship() {
       <Heading />
       <section className="bg-gray-100 max-w-7x1 mx-auto py-1 sm:py-5 lg:py-6 sm:px-6 lg:px-8">
         <div className="mt-10 sm:mt-0">
-          <div className="md:grid md:grid-cols-3 md:gap-6">
-            <div className="md:col-span-1">
-              <div className="px-4 sm:px-0">
-                <h3 className="text-lg font-medium leading-6 text-gray-900">
-                  Personal Information
-                </h3>
-                <p className="mt-1 text-sm text-gray-600">
-                  Enter your contact information here.
-                </p>
-              </div>
-            </div>
-            <div className="mt-5 md:mt-0 md:col-span-2">
-              <form id="application-form" onSubmit={handleSubmit}>
-                <div className="shadow overflow-hidden sm:rounded-md">
-                  <div className="px-4 py-5 bg-white sm:p-6">
-                    <div className="grid grid-cols-6 gap-6">
-                      <div className="col-span-6 sm:col-span-3">
-                        <label
-                          htmlFor="first_name"
-                          className="block text-sm font-medium text-gray-700"
-                        >
-                          First name
-                        </label>
-                        <input
-                          type="text"
-                          name="first_name"
-                          id="first_name"
-                          required
-                          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                        />
-                      </div>
+          <div className="mt-5 md:mt-0 md:w-9/12 mx-auto">
+            <form id="application-form" onSubmit={handleSubmit}>
+              <div className="shadow overflow-hidden sm:rounded-md">
+                <div className="px-4 py-5 bg-white sm:p-6">
+                  <div className="grid grid-cols-6 gap-6">
+                    <div className="col-span-6 sm:col-span-3">
+                      <label
+                        htmlFor="first_name"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        First name
+                      </label>
+                      <input
+                        type="text"
+                        name="first_name"
+                        id="first_name"
+                        required
+                        className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                      />
+                    </div>
 
-                      <div className="col-span-6 sm:col-span-3">
-                        <label
-                          htmlFor="last_name"
-                          className="block text-sm font-medium text-gray-700"
-                        >
-                          Last name
-                        </label>
-                        <input
-                          type="text"
-                          name="last_name"
-                          id="last_name"
-                          required
-                          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                        />
-                      </div>
+                    <div className="col-span-6 sm:col-span-3">
+                      <label
+                        htmlFor="last_name"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        Last name
+                      </label>
+                      <input
+                        type="text"
+                        name="last_name"
+                        id="last_name"
+                        required
+                        className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                      />
+                    </div>
 
-                      <div className="col-span-6 sm:col-span-3">
-                        <label
-                          htmlFor="email"
-                          className="block text-sm font-medium text-gray-700"
-                        >
-                          Email address
-                        </label>
-                        <input
-                          type="text"
-                          name="email"
-                          id="email"
-                          required
-                          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                        />
-                      </div>
+                    <div className="col-span-6 sm:col-span-3">
+                      <label
+                        htmlFor="email"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        Email address
+                      </label>
+                      <input
+                        type="text"
+                        name="email"
+                        id="email"
+                        required
+                        className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                      />
+                    </div>
 
-                      <div className="col-span-6 sm:col-span-3">
-                        <label
-                          htmlFor="phone"
-                          className="block text-sm font-medium text-gray-700"
-                        >
-                          Phone Number
-                        </label>
-                        <input
-                          type="text"
-                          name="phone"
-                          id="phone"
-                          required
-                          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                        />
-                      </div>
+                    <div className="col-span-6 sm:col-span-3">
+                      <label
+                        htmlFor="phone"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        Phone Number
+                      </label>
+                      <input
+                        type="text"
+                        name="phone"
+                        id="phone"
+                        required
+                        className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                      />
+                    </div>
 
-                      <div className="col-span-6 sm:col-span-6 lg:col-span-2">
-                        <label
-                          htmlFor="city"
-                          className="block text-sm font-medium text-gray-700"
-                        >
-                          City
-                        </label>
-                        <input
-                          type="text"
-                          name="city"
-                          id="city"
-                          required
-                          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                        />
-                      </div>
+                    <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+                      <label
+                        htmlFor="city"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        City
+                      </label>
+                      <input
+                        type="text"
+                        name="city"
+                        id="city"
+                        required
+                        className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                      />
+                    </div>
 
-                      <div className="col-span-6 sm:col-span-6 lg:col-span-2">
-                        <label
-                          htmlFor="school"
-                          className="block text-sm font-medium text-gray-700"
-                        >
-                          School
-                        </label>
-                        <input
-                          type="text"
-                          name="school"
-                          id="school"
-                          required
-                          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                        />
-                      </div>
+                    <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+                      <label
+                        htmlFor="school"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        School
+                      </label>
+                      <input
+                        type="text"
+                        name="school"
+                        id="school"
+                        required
+                        className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                      />
+                    </div>
 
-                      <div className="col-span-6 sm:col-span-6 lg:col-span-2">
-                        <label
-                          htmlFor="team"
-                          className="block text-sm font-medium text-gray-700"
-                        >
-                          BNS Team
-                        </label>
-                        <select
-                          id="team"
-                          name="team"
-                          required
-                          className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                        >
-                          <option>U15</option>
-                          <option>U17</option>
-                        </select>
-                      </div>
+                    <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+                      <label
+                        htmlFor="team"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        BNS Team
+                      </label>
+                      <select
+                        id="team"
+                        name="team"
+                        required
+                        className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                      >
+                        <option>U15</option>
+                        <option>U17</option>
+                      </select>
+                    </div>
 
-                      <div className="col-span-6">
-                        <label
-                          htmlFor="application"
-                          className="block text-sm font-medium text-gray-700"
-                        >
-                          Application Question
-                        </label>
-                        <p className="mt-2 text-sm text-gray-500">
-                          Please share how this scholarship will impact you -
-                          you may speak to how it will impact you during the
-                          season with BNS this summer, how it will impact your
-                          future basketball journey, and/or how it will impact
-                          you as a person off the basketball court. <br />
-                          <br />
-                          We'd love to hear about your aspirations both on and
-                          off the court. Andrew embodied strong qualities of
-                          work ethic, leadership, humbleness, and gratitude,
-                          allowing him to be a champion on and off the court. We
-                          encourage you to highlight qualities like these in
-                          yourself. In the words of Andrew, "Never forget what
-                          made you who you are." (Max. 500 words).
-                        </p>
-                        <div className="mt-5">
-                          <textarea
-                            id="application"
-                            name="application"
-                            rows="10"
-                            className="txt shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md whitespace-pre-wrap"
-                            placeholder="Your response here..."
-                            maxLength="3500"
-                            required
-                          ></textarea>
-                        </div>
+                    <div className="col-span-6">
+                      <label
+                        htmlFor="application"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        Application Question
+                      </label>
+                      <p className="mt-2 text-sm text-gray-500">
+                        Please share how this scholarship will impact you - you
+                        may speak to how it will impact you during the season
+                        with BNS this summer, how it will impact your future
+                        basketball journey, and/or how it will impact you as a
+                        person off the basketball court. <br />
+                        <br />
+                        We'd love to hear about your aspirations both on and off
+                        the court. Andrew embodied strong qualities of work
+                        ethic, leadership, humbleness, and gratitude, allowing
+                        him to be a champion on and off the court. We encourage
+                        you to highlight qualities like these in yourself. In
+                        the words of Andrew, "Never forget what made you who you
+                        are." (Max. 500 words).
+                      </p>
+                      <div className="mt-5">
+                        <textarea
+                          id="application"
+                          name="application"
+                          rows="10"
+                          className="txt shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md whitespace-pre-wrap"
+                          placeholder="Your response here..."
+                          maxLength="3500"
+                          required
+                        ></textarea>
                       </div>
                     </div>
                   </div>
-
-                  <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                    <button
-                      type="submit"
-                      className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    >
-                      {status}
-                    </button>
-                  </div>
                 </div>
-              </form>
-            </div>
+
+                <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                  <button
+                    type="submit"
+                    className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  >
+                    {status}
+                  </button>
+                </div>
+              </div>
+            </form>
           </div>
         </div>
       </section>
