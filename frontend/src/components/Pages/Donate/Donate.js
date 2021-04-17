2e7b231e3a97a6ac95c0606dda5c143e131f4255
Liam So <Liam.So@dal.ca>
2021-04-16 22:03:59 -0300

    Changed format

--- frontend/src/components/Pages/Donate/Donate.js
+++ frontend/src/components/Pages/Donate/Donate.js
@@ -1,38 +1,27 @@
 import React from "react";
 import Topbar from "../../Topbar/Topbar";
-import image from "../../../images/donate.JPG"
 
 function Donate() {
 
   return (
-    <div>
-      <Topbar transparent={true}/>
-      <div className="py-16">
-        <div className="container m-auto px-6">
+    <div className="donate">
+      <Topbar />
+      <div class="w-full h-screen flex justify-center items-center">
+        <div class="mx-4 text-center text-white">
+            <h1 class="font-bold text-4xl xl:text-6xl mb-4">Make a donation.</h1>
+            <h2 class="font-bold text-s lg:text-xl md:px-24 lg:px-60 xl:80 mb-12 ">If you would like to donate to our cause, you can do so through the links below. Your donation will go directly towards the Andrew Milner Memorial Scholarship Fund. This scholarship is presented to an athlete playing for Basketball Nova Scotia that truly embodies the character of Andrew- traits like work ethic, leadership, humbleness, and gratitude.</h2>
 
-        <div className="lg:flex justify-between items-center">
-            <div className="lg:w-6/12 lg:p-0 p-7">
-                <h1 className="text-2xl font-bold leading-tight mb-5 capitalize">  Thank you in advance for your generous support! </h1>
-                <p className="text-l">  If you would like to donate to our cause, you can do so through the links below. Your donation will go directly towards the Andrew Milner Memorial Scholarship Fund. This scholarship is presented to an athlete playing for Basketball Nova Scotia that truly embodies the character of Andrew- traits like work ethic, leadership, humbleness, and gratitude. </p>
-
-                <div className="py-4 flex justify-center md:justify-start">
-                    <form action="https://www.paypal.com/donate" method="post" target="_top">
+            <div className="get-app flex space-x-5 mt-10 justify-center">
+                <form action="https://www.paypal.com/donate" method="post" target="_top">
                       <input type="hidden" name="hosted_button_id" value="N7M2RLZ8WXB78" />
-                      <input className="h-32" type="image" src="https://www.svgrepo.com/show/86407/donate.svg" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+                      <input className="h-24" type="image" src="https://www.svgrepo.com/show/86407/donate.svg" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
                       <img alt="" border="0" src="https://www.paypal.com/en_CA/i/scr/pixel.gif" width="1" />
-                    </form>
-                </div>
-
+                </form>
               </div>
-              <div className="lg:w-5/12 order-2">
-                <img src={image}
-                alt="" className="rounded"/>
-              </div>
-          </div>
 
         </div>
-      </div>
-      </div>
+    </div>
+    </div>
   );
 }
 
