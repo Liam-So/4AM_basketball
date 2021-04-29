09a272e2d0457d622c39a41fa993885b27b25a2d
Ryan Hong <65246381+ryan-hong@users.noreply.github.com>
2021-04-29 19:31:14 -0300

    conditional button

--- frontend/src/components/Pages/HomePage/Home.js
+++ frontend/src/components/Pages/HomePage/Home.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { homeObjOne, homeObjTwo } from "./Data";
 import HeroSection from "../../HeroSection/HeroSection";
 import { Link } from "react-router-dom";
 import Topbar from "../../Topbar/Topbar";
 import { motion } from "framer-motion";
+import axios from "../../../axios";
 
 function Home() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const req = await axios.get("/registration");
+
+      setProducts(req.data);
+    }
+
+    fetchData();
+  }, []);
+
+  const availableProducts = products.filter((product) => product.sku > 0);
+
   return (
     <motion.section
       initial={{ opacity: 0 }}
@@ -21,7 +36,7 @@ function Home() {
             style={{ color: "#fff", textDecoration: "none" }}
             className="my-4 py-3 px-5 bg-yellow-500 rounded-full text-sm hover:bg-yellow-300 transition duration-300 ease-in-out flex items-center animate-bounce"
           >
-            DONATE NOW
+            {availableProducts.length ? "REGISTER NOW" : "DONATE NOW"}
           </Link>
         </div>
       </div>
