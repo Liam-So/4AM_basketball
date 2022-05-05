afff33b800bec2862aae3a8cb333a544c9c1be82
Liam So <Liam.So@dal.ca>
2022-05-05 15:29:17 -0300

    update nodemailer tokens

--- backend/config.js
+++ backend/config.js
@@ -4,9 +4,9 @@ const config = {
     "226073801380-bi37s04jqrbm0kajobdpen766rsht2vt.apps.googleusercontent.com",
   client_secret: "bTSi6K-cAhbpGpw-sne9FZwz",
   refresh_token:
-    "1//04Q3eg3N7OGRgCgYIARAAGAQSNwF-L9IrAt3liQ8bKzCwy5fsb4r9sl0souyxFb_mCHvSVjd3_SIR5LKrbnix57PyItX-mMc8EJQ",
+    "1//04LqRmueifDz-CgYIARAAGAQSNwF-L9IrF57q3TLNVXHgq7DFIbMm6VCHgP-YiEI_9H2qmZO6J1eAyz9I59eILvblMMeR4c43nJM",
   access_token:
-    "ya29.a0AfH6SMCLLt7KkywTOC6St466QCBM_nd8O-C7bc2NMIwXzrMBwx8vmHzYmbE-LlXU2HssJ9RpUdjlqIigqHLyMgxsUIYEuKbFyGZEFBboCMFGL_Gd8qjMX0PiX-iJAlxhZfFuZpwPZQ9luGUqp-L-IAP688Yy",
+    "ya29.A0ARrdaM9bI0oxsRwdwyOtTmIsh0lakemaDSKiI3B_fKA5rQ0WcFQTfbJmzYiqaTnywWFfspZeIxJrqkbj6ntvLIim4bFk9VoK90c0WbYNn8MsEkSl7n-3NPG9eCjD5WPLbt-i2lcZk8Ze2X7tb8pFrbqVhdpx",
 };
 
 export default config;
